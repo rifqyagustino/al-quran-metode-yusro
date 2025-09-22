88de64f374f7d6dee39b2c0eb8a37e022059fcb2
Moch Virgiawan Caesar Ridollohi <virgiawan.muhamad@gmail.com>
2025-09-22 10:02:14 +0700

    fix logo to Logo

--- src/app/(DashboardLayout)/layout/shared/logo/FullLogo.tsx
+++ src/app/(DashboardLayout)/layout/shared/logo/FullLogo.tsx
@@ -1,24 +1,24 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import Logo from "/public/images/logos/logo.png";
+import Logo from "/public/images/logos/Logo.png";
 import Link from "next/link";
 const FullLogo = () => {
   return (
     <Link href={"/"}>
       {/* Logo untuk tema terang */}
-      <Image 
-        src={Logo} 
-        alt="logo" 
-        className="block dark:hidden rtl:scale-x-[-1]" 
+      <Image
+        src={Logo}
+        alt="logo"
+        className="block dark:hidden rtl:scale-x-[-1]"
         width={200} // Atur lebar logo di sini
       />
-      
+
       {/* Logo untuk tema gelap */}
-      <Image 
+      <Image
         src={Logo} // Anda bisa gunakan logo yang sama atau yang berbeda
-        alt="logo" 
-        className="hidden dark:block rtl:scale-x-[-1]" 
+        alt="logo"
+        className="hidden dark:block rtl:scale-x-[-1]"
         width={200} // Atur lebar logo di sini
       />
     </Link>
